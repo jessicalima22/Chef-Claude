9a83930154d27c7482c0a57c57ce72b168032887
jessicalima22 <jessica.lima22@hotmail.com>
2025-01-28 00:07:04 -0300

    ai integration secret outout

---
+++ src/components/ClaudeRecipe.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable react/prop-types */
+export default function ClaudeRecipe(props) {
+    return (
+        <section>
+            <pre>{props.recipe}</pre>
+        </section>
+    )
+}

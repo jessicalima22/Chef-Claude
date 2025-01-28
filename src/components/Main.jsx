a5bfddfa627a21e3bbff59a75a9f8703fe872f66
jessicalima22 <jessica.lima22@hotmail.com>
2025-01-28 02:02:59 -0300

    restored

---
+++ src/components/Main.jsx
@@ -0,0 +1,48 @@
+import React from "react"
+import IngredientsList from "./IngredientsList"
+import ClaudeRecipe from "./ClaudeRecipe"
+import { getRecipeFromMistral } from "../utils/getRecipeFromMIstral"
+
+export default function Main() {
+    const [ingredients, setIngredients] = React.useState([])
+    const [recipe, setRecipe] = React.useState("")
+
+    async function getRecipe() {
+        const recipeMarkdown = await getRecipeFromMistral(ingredients)
+        setRecipe(recipeMarkdown)
+    }
+
+    function addIngredient(formData) {
+        const newIngredient = formData.get("ingredient")
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault(); // Evita que o formulário seja enviado de maneira tradicional
+        const formData = new FormData(event.target);
+        addIngredient(formData);
+    };
+
+    return (
+        <main>
+            <form onSubmit={handleSubmit} className="add-ingredient-form">
+                <input
+                    type="text"
+                    placeholder="e.g. oregano"
+                    aria-label="Add ingredient"
+                    name="ingredient"
+                />
+                <button>Add ingredient</button>
+            </form>
+
+            {ingredients.length > 0 &&
+                <IngredientsList
+                    ingredients={ingredients}
+                    getRecipe={getRecipe}
+                />
+            }
+
+            {recipe && <ClaudeRecipe recipe={recipe} />}
+        </main>
+    )
+}

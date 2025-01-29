4b91d9483896858c5b073b4f338418df844d0a56
jessicalima22 <jessica.lima22@hotmail.com>
2025-01-29 02:07:40 -0300

    Updates

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  base: '/chef-claude/', // Use "/" se for apenas para Vercel
-  plugins: [react()],
-});

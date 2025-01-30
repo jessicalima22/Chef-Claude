9fefb0ca3353e1c657f37f3acd655cc463f74517
jessicalima22 <jessica.lima22@hotmail.com>
2025-01-30 17:58:07 -0300

    url testing 3

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '', // Use "/" se for apenas para Vercel
   plugins: [react()],
-});
+  base: '/Chef-Claude/', // Nome do seu repositório GitHub
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true
+  }
+})

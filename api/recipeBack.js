803817facf5efdbfad24616fd8a697b8761b8b9f
jessicalima22 <jessica.lima22@hotmail.com>
2025-01-30 19:30:55 -0300

    url vercel 5

--- api/recipeBack.js
+++ api/recipeBack.js
@@ -5,14 +5,28 @@ const SYSTEM_PROMPT = `You are an assistant that receives a list of ingredients.
 export default async function handler(req, res) {
     console.log('API Called');
 
-    // Configuração CORS
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,POST');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    const allowedOrigins = [
+        'http://localhost:5173',
+        'http://localhost:3000',
+        'https://jessicalima22.github.io/Chef-Claude/',
+        'https://chef-claude-git-main-jessicalima22s-projects.vercel.app/'
+    ];
+    
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    } else {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+    }
+
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
 
     if (req.method === 'OPTIONS') {
         return res.status(200).end();
+    } if (req.method === 'GET') {
+        return res.status(200).json({ message: 'API is working!' });
     }
 
     try {

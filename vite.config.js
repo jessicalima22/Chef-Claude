ca4988ef232298c99cfc9297b3126a114f6a3e5c
jessicalima22 <jessica.lima22@hotmail.com>
2025-01-30 04:13:11 -0300

    removing / vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', // Use "/" se for apenas para Vercel
+  base: '', // Use "/" se for apenas para Vercel
   plugins: [react()],
 });
